8fae2dcf1086b82f98634a51edae1848e73d331a
Madrugamatos <mauricio_matoss@outlook.com>
2023-10-21 19:14:59 -0300

    trabalho php

---
+++ projetos-php/index.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Top 1 Series</title>
+        <link rel="stylesheet" href="style.css">
+    </head>
+    <body>
+        
+        <nav>
+            <img src="imagem/logo-netflix.png" alt="">
+            <a href="#">Home</a>
+            <a href="login.html">Login</a>
+            <a href="#">Generos</a>
+            <a href="#">Contato</a>
+            <a href="#">Descrição</a>
+
+        </nav>
+
+        <main>
+            <h1>Ranking de melhores séries por gênero.</h1>
+        
+            
+            <div id="apresentacao">
+                
+                <p>Bem-vindo ao Top 1 Séries, o seu portal definitivo para explorar o emocionante universo das séries de TV. Navegue por uma pequena coleção de séries em nosso catálogo, desde os dramas mais intensos até as comédias mais hilariantes.</p>
+
+                
+
+            </div>
+
+
+            <div id="generos">
+                <img src="imagem/series.jpeg" alt="">
+            <!--<ul>
+            <li><a href="#">Drama</a></li>
+            <li><a href="#">Comédia</a></li>
+            <li><a href="#">Ficção Cientifica</a></li>
+            </ul>
+            -->
+            </div>
+        </main>
+    </body>
+</html>
